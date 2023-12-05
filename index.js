54ee4918beb36b4882c996383d4a365f01f7f022
Mohammmedrafique <pushpendrasharmakarauli@gmail.com>
2023-12-05 22:19:29 +0530

    d

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const { router } = require("./Route/api");
+require("dotenv").config();
+const app = express();
+const PORT = 8080;
+
+// Enable CORS
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+
+// Use API routes
+app.use("/", router);
+
+// Start the server
+app.listen(PORT, async () => {
+  await mongoose.connect(process.env.DB);
+  console.log(`Server is running on port ${PORT} and connected toDB`);
+});
